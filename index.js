760a3f4efec8aca9337eb4a47434cd7ecf7871dc
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 17:28:44 +0900

    gets ip, agent, and lang

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  // console.log(JSON.stringify(req.socket.remoteAddress));
+  res.json({ipaddress: req.socket.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent']})
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
